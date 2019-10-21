b113a3ca3cee0f2bfdf01c6359bb4d1001020507
Jon Bash <jonbashmusic@gmail.com>
2019-10-21 09:34:56 -0700

    add storyboard scenes & view controllers

---
+++ Countdowns/Countdowns/Helpers/StringExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  StringExtensions.swift
+//  Countdowns
+//
+//  Created by Jon Bash on 2019-10-21.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    static let countdownCellReuseID = "CountdownCell"
+}
---
+++ Countdowns/Countdowns/Views/CountdownTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountdownTableViewCell.swift
+//  Countdowns
+//
+//  Created by Jon Bash on 2019-10-21.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import UIKit
+
+class CountdownTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
