5dc152c979ac956f20325ecd8f3cbd779790da5e
Jon Bash <jonbashmusic@gmail.com>
2019-10-22 10:50:51 -0700

    implement Tag as string typealias

---
+++ Countdowns/Countdowns/Models/Tag.swift
@@ -0,0 +1,11 @@
+//
+//  Tag.swift
+//  Countdowns
+//
+//  Created by Jon Bash on 2019-10-22.
+//  Copyright © 2019 Jon Bash. All rights reserved.
+//
+
+import Foundation
+
+typealias Tag = String
