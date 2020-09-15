42d2bb3f19f92693bb678a6c41b0d724bef13a87
Jon Bash <jonbash@protonmail.com>
2020-09-15 07:23:47 -0700

    add test files

---
+++ CountdownsTests/CountdownsViewModelTests.swift
@@ -0,0 +1,15 @@
+//
+//  CountdownsViewModelTests.swift
+//  CountdownsTests
+//
+//  Created by Jon Bash on 2020-09-15.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import XCTest
+@testable import Countdowns
+
+
+class CountdownsViewModelTests: XCTestCase {
+
+}
---
+++ CountdownsTests/ViewModelntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModelntegrationTests.swift
+//  CountdownsTests
+//
+//  Created by Jon Bash on 2020-09-15.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+import XCTest
+@testable import Countdowns
+
+class ViewModelIntegrationTests: XCTestCase {
+
+}
